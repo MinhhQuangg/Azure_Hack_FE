71d7f37b3c7f2073819654a358dd35ac4ae8cfa2
linhphan03 <phanthuylinh221203@gmail.com>
2025-03-25 21:27:12 -0400

    temp

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -128,6 +128,8 @@ const ChatRoom = () => {
   };
 
   const { chatId: urlChatId, inviteLink } = useParams();
+  console.log(urlChatId, inviteLink);
+
   const navigate = useNavigate();
   //   const [chats, setChats] = useState(
   //     CHATS_DATA.map((chat) => ({
